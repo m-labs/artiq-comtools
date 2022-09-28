1ef3adc567c217e9da12f1f9183424aab7da1e9d
Brad Bondurant <brad.bondurant@duke.edu>
2022-09-28 12:26:50 -0400

    flake: add aarch64 support via flake-utils

--- flake.nix
+++ flake.nix
@@ -4,28 +4,31 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
   inputs.sipyco.url = github:m-labs/sipyco;
   inputs.sipyco.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = github:numtide/flake-utils;
 
-  outputs = { self, nixpkgs, sipyco }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      deps = [ pkgs.python3Packages.numpy pkgs.python3Packages.aiohttp sipyco.packages.x86_64-linux.sipyco ];
-    in rec {
-      packages.x86_64-linux = {
-        artiq-comtools = pkgs.python3Packages.buildPythonPackage {
-          pname = "artiq-comtools";
-          version = "1.1";
-          src = self;
-          propagatedBuildInputs = deps;
+  outputs = { self, nixpkgs, sipyco, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = [ pkgs.python3Packages.numpy pkgs.python3Packages.aiohttp sipyco.packages.${system}.sipyco ];
+      in rec {
+        packages = {
+          artiq-comtools = pkgs.python3Packages.buildPythonPackage {
+            pname = "artiq-comtools";
+            version = "1.1";
+            src = self;
+            propagatedBuildInputs = deps;
+          };
         };
-      };
 
-      defaultPackage.x86_64-linux = packages.x86_64-linux.artiq-comtools;
+        defaultPackage = packages.artiq-comtools;
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        name = "artiq-comtools-dev-shell";
-        buildInputs = [
-          (pkgs.python3.withPackages(ps: deps))
-        ];
-      };
-    };
-}
+        devShell = pkgs.mkShell {
+          name = "artiq-comtools-dev-shell";
+          buildInputs = [
+            (pkgs.python3.withPackages(ps: deps))
+          ];
+        };
+      }
+    );
+  }
